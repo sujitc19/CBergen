4f5de886da43683d0c1704c7b85534f46c0e61f6
sujitc19 <sujitc19@gmail.com>
2018-11-28 19:51:11 -0500

    Added email contact sending
    Added fields for sending email, currently set to a non real email.

--- CarolineBergen/Controllers/HomeController.cs
+++ CarolineBergen/Controllers/HomeController.cs
@@ -7,11 +7,14 @@ using Microsoft.AspNetCore.Mvc;
 using CarolineBergen.Models;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc.Localization;
+using System.Net.Mail;
+using System.Net;
 
 namespace CarolineBergen.Controllers
 {
     public class HomeController : Controller
     {
+        private const string personalToMail = "thisisnotarealemail@gmail.com";//"splitkegan@gmail.com";//"sujitc19@gmail.com";//caroline.bergen@outlook.com
 
         public IActionResult Index()
         {
@@ -26,11 +29,62 @@ namespace CarolineBergen.Controllers
             return View();
         }
 
-        public IActionResult Contact()
+        [HttpGet]
+        public async Task<IActionResult> Contact()
+        {
+            if (TempData["EmailSentStatus"] != null)
+            {
+                ViewBag.EmailSentStatus = TempData["EmailSentStatus"];
+            }
+            else
+            {
+                ViewBag.EmailSentStatus = false;
+            }
+            ViewData["Message"] = "Contact Information";
+            return View(new ContactViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Contact([FromForm] ContactViewModel model)
         {
             ViewData["Message"] = "Contact Information";
+            if (!ModelState.IsValid)
+            {
+                ViewBag.EmailSentStatus = false;
+                return View();
+            }
+            else
+            {
+                MailAddress From = new MailAddress(model.Email);
+                MailAddress To = new MailAddress(personalToMail);
+                MailMessage message = new MailMessage(From, To);
+                message.Subject = $"{model.Name}'s Request via site mailer";
+                message.Body = model.BodyMessage;
 
-            return View();
+                try
+                {
+                    var smtp = new System.Net.Mail.SmtpClient();
+                    {
+                        smtp.Host = "smtp.gmail.com";
+                        smtp.Port = 587;
+                        smtp.Credentials = new NetworkCredential(model.Email, model.Password);
+                        smtp.EnableSsl = true;
+                        smtp.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+                        smtp.Timeout = 20000;
+                    }
+                    
+                    smtp.Send(message);
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.ErrorMessage = ex.Message + "\n" + ex.StackTrace;
+                    ViewBag.EmailSentStatus = false;
+                    return View();
+                }
+
+                TempData["EmailSentStatus"] = true;
+                return RedirectToAction("Contact");
+            }
         }
 
         public IActionResult Error()
---
+++ CarolineBergen/Models/ContactViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarolineBergen.Models
+{
+    public class ContactViewModel
+    {
+        [Required]
+        [Display(Name = "Name")]
+        public string Name
+        { get; set; }
+        [Required]
+        [Display(Name = "Email")]
+        [EmailAddress(ErrorMessage = "Invalid Email.")]
+        public string Email
+        { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password
+        { get; set; }
+        [Required(ErrorMessage = "Please include a description.")]
+        [Display(Name="Tell us about your project")]
+        public string BodyMessage
+        { get; set; }
+
+    }
+}
